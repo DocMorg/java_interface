42c548997fe80fbcce4734b0a56c3a4923788dd4
DocMorg <vlad_ustimov@mail.ru>
2020-04-06 02:28:44 +0300

    added copies of existing code for refactoring to OOP paradigm

---
+++ src/com/company/TodoListOOP.java
@@ -0,0 +1,154 @@
+package com.company;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class TodoListOOP extends Application {
+
+    private String filename = "/save.txt";
+    private ObservableList<TodosOOP> table_view_data;
+
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
+    @Override public void start(Stage stage) throws IOException {
+        URL path_to_file = getClass().getResource(this.filename);
+        if (path_to_file==null){
+            System.out.println("FileNotFound error");
+            System.exit(1);
+        }
+        this.table_view_data = FXCollections.observableArrayList(
+                Files.readAllLines(new File(path_to_file.getPath()).toPath())
+                        .stream()
+                        .map(line -> {
+                            String[] details = line.split(",");
+                            return new TodosOOP(details[0],details[1],details[2]);
+                        })
+                        .collect(Collectors.toList()));
+
+        ObservableList<String> list_view_data = FXCollections.observableArrayList();
+        for (TodosOOP value : this.table_view_data) {
+            list_view_data.add(value.getName());
+        }
+        ListView<String> listView = new ListView<>(list_view_data);
+        listView.setPrefWidth(235);
+        listView.setPrefHeight(200);
+
+        Button addButton = new Button();
+        addButton.setText("Add");
+
+
+        TableView<TodosOOP> table = new TableView<>(this.table_view_data);
+        table.setPrefWidth(235);
+        table.setPrefHeight(200);
+
+        TableColumn<TodosOOP, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        nameColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<TodosOOP, String> t) -> {
+                    t.getTableView().getItems().get(
+                            t.getTablePosition().getRow()).setName(t.getNewValue());
+                    int row_id = t.getTableView().getSelectionModel().getSelectedIndex();
+                    listView.getItems().set(row_id, t.getNewValue());
+                });
+        table.getColumns().add(nameColumn);
+
+        TableColumn<TodosOOP, String> timeColumn = new TableColumn<>("Date");
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("Date"));
+        timeColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        timeColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<TodosOOP, String> t) -> t.getTableView().getItems().get(
+                        t.getTablePosition().getRow()).setDate(t.getNewValue()));
+        table.getColumns().add(timeColumn);
+
+        TableColumn<TodosOOP, String> descrColumn = new TableColumn<>("Description");
+        descrColumn.setCellValueFactory(new PropertyValueFactory<>("Description"));
+        descrColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        descrColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<TodosOOP, String> t) -> t.getTableView().getItems().get(
+                        t.getTablePosition().getRow()).setDescription(t.getNewValue()));
+        table.getColumns().add(descrColumn);
+
+        TextField inputField = new TextField();
+
+        addButton.setOnAction(e -> {
+            this.table_view_data.addAll(FXCollections.observableArrayList(new TodosOOP(inputField.getText())));
+            list_view_data.add(inputField.getText());
+            inputField.setText("");
+            inputField.requestFocus();
+        });
+
+        addButton.disableProperty()
+                .bind(Bindings.isEmpty(inputField.textProperty()));
+
+        Button removeButton = new Button();
+        removeButton.setText("Remove");
+
+        removeButton.setOnAction(e -> {
+            if (table.getSelectionModel().getSelectedItem() != null ||
+                    listView.getSelectionModel().getSelectedItem() != null){
+                final int idToRemove = listView.getSelectionModel().getSelectedIndex();
+                final int idToRemove1 = table.getSelectionModel().getSelectedIndex();
+                if (idToRemove1 == -1) {
+                    listView.getItems().remove(idToRemove);
+                    table.getItems().remove(idToRemove);
+                } else {
+                    listView.getItems().remove(idToRemove1);
+                    table.getItems().remove(idToRemove1);
+                }
+            }
+        });
+        table.setEditable(true);
+        listView.setEditable(true);
+        HBox entryBox = new HBox();
+        entryBox.getChildren().addAll(inputField, addButton, removeButton);
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(listView, table, entryBox);
+
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+        stage.setTitle("TodosOOP");
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws IOException {
+        Writer writer = null;
+        try {
+            URL path_to_file = getClass().getResource(this.filename);
+            if (path_to_file==null){
+                System.out.println("FileNotFound error");
+                System.exit(1);
+            }
+            writer = new BufferedWriter(new FileWriter(new File(path_to_file.getPath())));
+            for (TodosOOP line : this.table_view_data) {
+                String text = line.getName() + "," + line.getDate() + "," + line.getDescription() + "\n";
+                writer.write(text);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            assert writer != null;
+            writer.flush();
+            writer.close();
+        }
+    }
+}
---
+++ src/com/company/TodosOOP.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javafx.beans.property.*;
+
+
+public class TodosOOP {
+    private SimpleStringProperty name;
+    private SimpleStringProperty date;
+    private SimpleStringProperty description;
+
+    public String getName(){
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String value){
+        name.set(value);
+    }
+
+    public StringProperty dateProperty() {
+        return date;
+    }
+
+    public String getDate(){
+        return date.get();
+    }
+
+    public void setDate(String value){
+        date.set(value);
+    }
+
+    public String getDescription(){
+        return description.get();
+    }
+
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    public void setDescription(String value){
+        description.set(value);
+    }
+
+    public TodosOOP(){
+        this.name = new SimpleStringProperty("");
+        this.date = new SimpleStringProperty("");
+        this.description = new SimpleStringProperty("");
+    }
+    public TodosOOP(String name, String date, String description) {
+        this.name = new SimpleStringProperty(name);
+        this.date = new SimpleStringProperty(date);
+        this.description = new SimpleStringProperty(description);
+    }
+
+    public TodosOOP(String name) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        this.name = new SimpleStringProperty(name);
+        this.date = new SimpleStringProperty(dateFormat.format(tomorrow));
+        this.description = new SimpleStringProperty(name);
+    }
+
+    public TodosOOP(String name, String description){
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        this.name = new SimpleStringProperty(name);
+        this.date = new SimpleStringProperty(dateFormat.format(tomorrow));
+        this.description = new SimpleStringProperty(description);
+    }
+
+}
