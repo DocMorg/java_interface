523b1dbb61441f73455de60a7758c48939f27c40
DocMorg <vlad_ustimov@mail.ru>
2020-04-14 21:13:41 +0300

    made properties final deleted unused getters added loadFromFile & printline functions added imports of javafx lib

--- src/com/company/TodosOOP.java
+++ src/com/company/TodosOOP.java
@@ -1,56 +1,55 @@
 package com.company;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import javafx.beans.property.*;
+import java.util.stream.Collectors;
 
 
 public class TodosOOP {
-    private SimpleStringProperty name;
-    private SimpleStringProperty date;
-    private SimpleStringProperty description;
+    private final SimpleStringProperty name;
+    private final SimpleStringProperty date;
+    private final SimpleStringProperty description;
+
 
     public String getName(){
         return name.get();
     }
-
-    public StringProperty nameProperty() {
-        return name;
+    public String getDate(){
+        return date.get();
+    }
+    public String getDescription(){
+        return description.get();
     }
 
     public void setName(String value){
         name.set(value);
     }
 
-    public StringProperty dateProperty() {
-        return date;
-    }
-
-    public String getDate(){
-        return date.get();
-    }
-
     public void setDate(String value){
         date.set(value);
     }
 
-    public String getDescription(){
-        return description.get();
-    }
-
-    public StringProperty descriptionProperty() {
-        return description;
-    }
 
     public void setDescription(String value){
         description.set(value);
     }
 
-    public TodosOOP(){
+
+    public TodosOOP() {
         this.name = new SimpleStringProperty("");
         this.date = new SimpleStringProperty("");
         this.description = new SimpleStringProperty("");
     }
+
     public TodosOOP(String name, String date, String description) {
         this.name = new SimpleStringProperty(name);
         this.date = new SimpleStringProperty(date);
@@ -65,12 +64,27 @@ public class TodosOOP {
         this.description = new SimpleStringProperty(name);
     }
 
-    public TodosOOP(String name, String description){
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate tomorrow = LocalDate.now().plusDays(1);
-        this.name = new SimpleStringProperty(name);
-        this.date = new SimpleStringProperty(dateFormat.format(tomorrow));
-        this.description = new SimpleStringProperty(description);
+    public ObservableList<TodosOOP> loadFromFile(@NotNull URL filename) throws IOException {
+        return FXCollections.observableArrayList(
+                Files.readAllLines(new File(filename.getPath()).toPath())
+                        .stream()
+                        .map(line -> {
+                            String[] details = line.split(",");
+                            return new TodosOOP(details[0],details[1],details[2]);
+                        })
+                        .collect(Collectors.toList()));
+    }
+
+    public String printline(@NotNull TodosOOP line){
+        return line.name.getValue() + "," + line.date.getValue() + "," + line.description.getValue() + "\n";
+    }
+
+    public ObservableList<String> fillNamesIntoList(ObservableList<TodosOOP> data){
+        ObservableList<String> list_view_data = FXCollections.observableArrayList();
+        for (TodosOOP value : data) {
+            list_view_data.add(value.getName());
+        }
+        return list_view_data;
     }
 
 }
