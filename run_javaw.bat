570bf18311decc12dcda341b0418208ae01d94a5
Alex Ovchinnikov <SanSeyp6@gmail.com>
2020-04-15 23:57:04 +0300

    Reverted start /b before javaw invocation

--- run_javaw.bat
+++ run_javaw.bat
@@ -4,4 +4,4 @@ set MODULE_PATH=%userprofile%/.m2/repository/org/openjfx/javafx-base/11.0.2/java
 %userprofile%/.m2/repository/org/openjfx/javafx-controls/11.0.2/javafx-controls-11.0.2-win.jar;^
 %userprofile%/.m2/repository/org/openjfx/javafx-graphics/11.0.2/javafx-graphics-11.0.2-win.jar;
 
-javaw --module-path "target/classes;%MODULE_PATH%" -m com.company/com.company.TodoListOOP
+start /b javaw --module-path "target/classes;%MODULE_PATH%" -m com.company/com.company.TodoListOOP
