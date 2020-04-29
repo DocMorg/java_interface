8b8e204c264b82a3c1a51ad2fce9267fdf7197bd
DocMorg <vlad_ustimov@mail.ru>
2020-04-30 01:42:17 +0300

    fixed bug with super method. added emptying of TextField after reading it

--- src/main/java/todos/gui/MyJTextField.java
+++ src/main/java/todos/gui/MyJTextField.java
@@ -8,16 +8,6 @@ import javax.swing.*;
 public class MyJTextField extends JTextField {
 
     @Override
-    public String getText() {
-        try {
-            throw new Exception("This method is not supposed to be used due to encapsulation rules");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.toString();
-        }
-    }
-
-    @Override
     public void setText(String t){
     }
 
@@ -26,10 +16,27 @@ public class MyJTextField extends JTextField {
     }
 
     public Todos readText() throws EmptyFieldException {
-        if (this.getText().length() > 0) {
-            return new Todos(this.getMyText());
+        if (this.getMyText().length() > 0) {
+            String text = this.getMyText();
+            this.emptyTextField();
+            return new Todos(text);
+
         } else {
             throw new EmptyFieldException();
+        }
+    }
+
+    private void emptyTextField(){
+        super.setText("");
+    }
+
+    @Override
+    public String getText() {
+        try {
+            throw new Exception("This method is not supposed to be used due to encapsulation rules");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
         }
     }
 
