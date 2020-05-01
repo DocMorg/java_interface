8933600d9e9fc4f64a39fd4bee4ceed1241e965b
DocMorg <vlad_ustimov@mail.ru>
2020-05-01 21:57:04 +0300

    renamed from Todos removed unnecessary imports dropped empty constructor added a prototype for saving and loading data features

--- src/main/java/todos/core/DefaultTodo.java
+++ src/main/java/todos/core/DefaultTodo.java
@@ -1,29 +1,18 @@
 package todos.core;
 
-import java.io.*;
-import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Vector;
-import java.util.stream.Collectors;
-import javax.swing.*;
 
-public class Todos {
+public class DefaultTodo implements Todo {
     private final String name;
     private final String date;
 
-
-    public Todos() {
-        this.name = "";
-        this.date = "";
-    }
-
-    public Todos(String name, String date) {
+    public DefaultTodo(String name, String date) {
         this.name = name;
         this.date = date;
     }
 
-    public Todos(String name) {
+    public DefaultTodo(String name) {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate tomorrow = LocalDate.now().plusDays(1);
         this.name = name;
@@ -34,6 +23,19 @@ public class Todos {
     public String toString() {
         return this.name + "    " + this.date;
     }
+
+    @Override
+    public void saveTodo(Saver saver) {
+        saver.add("name", this.name);
+        saver.add("date", this.date);
+        saver.save();
+    }
+
+//    @Override
+//    public Todo loadTodo() {
+//        return this;
+//    }
+    
 
     //    private File file(String filename){
 //        return new File((getClass().getClassLoader().getResource(filename).getFile()));
@@ -54,23 +56,23 @@ public class Todos {
 //        return new File((getClass().getClassLoader().getResource(filename).getFile()));
 //    }
 //
-//    public JList<Todos> loadFromFile(String filename) throws IOException {
-//        return new JList<Todos>(
-//                (Vector<? extends Todos>) Files.readAllLines(file(filename).toPath())
+//    public JList<DefaultTodo> loadFromFile(String filename) throws IOException {
+//        return new JList<DefaultTodo>(
+//                (Vector<? extends DefaultTodo>) Files.readAllLines(file(filename).toPath())
 //                        .stream()
 //                        .map(line -> {
 //                            String[] data = line.split(",");
-//                            return new Todos(data[0], data[1], data[2]);
+//                            return new DefaultTodo(data[0], data[1], data[2]);
 //                        })
 //                        .collect(Collectors.toList()));
 //    }
 //
-//    public void writeData(ObservableList<Todos> data, String filename) throws IOException {
+//    public void writeData(ObservableList<DefaultTodo> data, String filename) throws IOException {
 //        if (data == null || filename == null){
 //            throw new IllegalArgumentException("data не должна быть null");
 //        }
 //        Writer writer = new BufferedWriter(new FileWriter(file(filename)));
-//        for (Todos line : data) {
+//        for (DefaultTodo line : data) {
 //            String text = line.getName() + "," + line.getDate() + "," + line.getDescription() + "\n";
 //            writer.write(text);
 //        }
@@ -78,9 +80,9 @@ public class Todos {
 //        writer.close();
 //    }
 //
-//    public ObservableList<String> fillNamesIntoList(ObservableList<Todos> data){
+//    public ObservableList<String> fillNamesIntoList(ObservableList<DefaultTodo> data){
 //        ObservableList<String> list_view_data = FXCollections.observableArrayList();
-//        for (Todos value : data) {
+//        for (DefaultTodo value : data) {
 //            list_view_data.add(value.getName());
 //        }
 //        return list_view_data;
