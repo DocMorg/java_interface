1da398d964313ade220f63be30a040cb80c5f2dd
DocMorg <vlad_ustimov@mail.ru>
2020-05-01 22:10:34 +0300

    aftermaths of renaming classes. dropped ListModel

--- src/main/java/todos/core/TodoList.java
+++ src/main/java/todos/core/TodoList.java
@@ -1,48 +1,16 @@
 package todos.core;
 
-import todos.gui.TodoListModel;
+public interface TodoList {
 
-import javax.swing.*;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
-public class TodoList implements TodoInterface {
+    void remove(int i);
+    void add(Todo item);
+    int getSize();
+    Todo getElementAt(int i);
+    Todo loadTodo();
 
-    public TodoList(){
-        this.todoListModel = new TodoListModel(this);
-    }
+    void saveTodo(Saver saver);
 
-    private final TodoListModel todoListModel;
-    private final List<Todos> list = new LinkedList<>();
 
-    public Iterator<Todos> iterator() {
-        return list.iterator();
-    }
-
-    public void remove(int i) {
-        if (i >= 0 && i < this.list.size()) {
-            this.list.remove(i);
-        }
-        this.todoListModel.remove(i);
-    }
-
-    public void add(Todos item) {
-        this.list.add(item);
-        this.todoListModel.add(this.getSize() - 1);
-    }
-
-    public Todos getElementAt(int i) {
-        return list.get(i);
-    }
-
-    public int getSize() {
-        return list.size();
-    }
-
-    @Override
-    public ListModel<Todos> getModel() {
-        return this.todoListModel;
-    }
 
 }
