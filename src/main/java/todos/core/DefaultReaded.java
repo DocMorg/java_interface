1c102404df2592be7693bf80c58b1a06d03f5752
DocMorg <vlad_ustimov@mail.ru>
2020-05-04 04:06:34 +0300

    created implementation of Readed interface initialized with FileInputStream it reads all data string by string, separating each by two and saving into List of String Arrays. A little parsing

---
+++ src/main/java/todos/core/DefaultReaded.java
@@ -0,0 +1,43 @@
+package todos.core;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class DefaultReaded implements Readed {
+
+    private final FileInputStream in;
+
+    public DefaultReaded(FileInputStream in) {
+        this.in = in;
+    }
+
+    @Override
+    public List<String[]> read() {
+        String line;
+        String csvSplitBy = ",";
+        LinkedList<String[]> list = new LinkedList<>();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(this.in))) {
+
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String[] newline = line.split(csvSplitBy);
+                newline[0] = newline[0].replace("\"", "");
+                newline[1] = newline[1].replace("\"", "");
+                list.add(newline);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    void main(){
+
+    }
+}
