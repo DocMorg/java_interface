8cf783dc8a38571a20501266377ef353e7785730
DocMorg <vlad_ustimov@mail.ru>
2020-05-09 00:45:19 +0300

    test XmlOutput

---
+++ src/main/java/todos/core/Outputs/XmlOutput.java
@@ -0,0 +1,56 @@
+package todos.core.Outputs;
+
+import org.xembly.Directives;
+import org.xembly.ImpossibleModificationException;
+import org.xembly.Xembler;
+
+import java.io.PrintStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class XmlOutput implements Output {
+
+    private final PrintStream out;
+    private final Map<String, String> map = new LinkedHashMap<>();
+    private String xml;
+    private String buffer;
+
+    public XmlOutput(PrintStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public Output addHeader(String header) {
+        return this;
+    }
+
+    @Override
+    public Output add(String name, String value) {
+        if (name.equals("name")){
+            this.buffer = value;
+        } else{
+            this.map.put(buffer, value);
+        }
+        return this;
+    }
+
+    @Override
+    public void save() {
+        Directives directives = new Directives().add("data");
+
+        for (Map.Entry<String, String> entry : map.entrySet()){
+            directives.add("item")
+                    .add("name").set(entry.getKey())
+                    .up()
+                    .add("date").set(entry.getValue())
+                    .up()
+                    .up();
+
+        }
+        try {
+            out.print(new Xembler(directives).xml());
+        } catch (ImpossibleModificationException e) {
+            e.printStackTrace();
+        }
+    }
+}
